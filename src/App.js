32fadffaef97c93b2fe026933696acef531291ad
davidlpc1 <davipadro76@gmail.com>
2020-11-14 12:50:09 -0300

    Simulação Pronta

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Slider from './components/Slider';
 import Aside from './components/Aside'
@@ -8,7 +8,41 @@ import next from './assets/img/next.svg';
 
 function App() {
   const Logo = "https://minhaconta.orama.com.br/static/img/logos/orama.svg";
+  const [AtualButton, setAtualButton] = useState(0);
+  const descriptionButtons = [
+    'A Home é a área inicial da aplicação',
+    'Conta que tem transações,é movimentada',
+    'Invista seu dinheiro aqui',
+    'Confira a cotação das moedas...',
+    'Acesse as ferramentas Orama...',
+    'Aprenda mais sobre finanças',
+    'Entre em contato com o nosso time'
+  ]
+
+  function simulation( { target }  ){
+    const { parentNode } = target;
+    const lenButtons = parentNode.previousElementSibling.childElementCount
+    parentNode.childNodes[1].innerHTML = descriptionButtons[AtualButton]
+    console.log(AtualButton >= lenButtons)
+    if(AtualButton > 0 && AtualButton < lenButtons){
+      parentNode.previousElementSibling.childNodes[AtualButton - 1].style.backgroundColor = '#639c3199';
+    }else if(AtualButton >= lenButtons){
+      parentNode.childNodes[1].innerHTML = 'Acabou a simulação';
+      setAtualButton(0);
+      finishSimulation({target});
+      return;
+    }
+    parentNode.previousElementSibling.childNodes[AtualButton].style.backgroundColor = '#639c31';
+    setAtualButton(AtualButton + 1);
+    parentNode.parentNode.parentNode.classList.add('popup')
+  }
   
+  function finishSimulation( {target } ){
+    localStorage.setItem('skipSimuladorOrama',String(true))
+    target.parentNode.style.display = 'none';
+    target.parentNode.parentNode.parentNode.classList.remove('popup')
+  }
+
   function setSkipTutorial(){
     localStorage.setItem('skipTutorialOrama',String(true));
   }
@@ -39,23 +73,17 @@ function App() {
       {Boolean(localStorage.getItem('skipTutorialOrama')) && !Boolean(localStorage.getItem('skipSimuladorOrama')) && (
         <>
           <Aside />
-          <div className="w-40 h-40 bg-gray-400 p-2 mx-64 my-4 text-justify text-black">
+          <div className="w-40 h-40 bg-gray-400 p-2 mx-64 my-4 text-black">
             <img className="h-4  cursor-pointer" 
-              onClick={( {target} ) => {
-                target.parentNode.style.display = 'none';
-                target.parentNode.parentNode.parentNode.classList.remove('popup')
-              }} 
+              onClick={finishSimulation} 
               src={close} alt="Close" />
-            <p className="text-sm">Inicie a simulação</p>
+            <p className="text-xl tracking-normal">Inicie a simulação</p>
             <img className="h-4  cursor-pointer mx-auto my-2" 
-              onClick={( {target} ) => {
-                target.parentNode.childNodes[1].innerHTML = 'A Home e´o início da Orama'
-                target.parentNode.parentNode.parentNode.classList.add('popup')
-              }} 
+              onClick={simulation} 
               src={next} alt="Next" />
           </div>
         </>
-      )}
+      )} 
 
     </div>
   );
